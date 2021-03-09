467214ee9b629a4b5ed975d9aa0cbf7c93b9a002
LazaroMPerez <lazpz61@gmail.com>
2021-03-09 16:06:17 -0500

    Changed game to slot machine

---
+++ app-slot-machine.js
@@ -0,0 +1,24 @@
+// Slot machine game, Utilize above to be a base idea of what i want the program to give an output on 
+
+
+// const slotMachine = slot => {
+// let container = []; 
+// Object.keys(slot).forEach(key => {
+// for(let i = 0; i < slot[key]; i++) {
+// container.push(key);
+// } 
+// })
+// return container[(Math.random() * container.length) | 0];
+// }
+
+// const slots = {
+// "bannana,bannana,bannana" : 20,
+// "apple,apple,apple": 10,
+// "orange,orange,oragne": 30,
+// "apple,orange,bannana": 2000,
+// "orange,bannana,apple":3000,
+// "bannana,apple,orange":3000
+// }
+
+// weightedLottery(slots);
+
