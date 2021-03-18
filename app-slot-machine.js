3241e2b19339e96fa888b1ec85c35cd4c6a3e987
LazaroMPerez <lazpz61@gmail.com>
2021-03-18 01:16:51 -0400

    Game Finished at current state

--- app-slot-machine.js
+++ app-slot-machine.js
@@ -1,3 +1,4 @@
+// All of my constants to grab the html id's to be able to make the changes once they are clicked
 const wallet = document.getElementById("withdrawl-wallet");
 const slotPics = document.getElementById("slot-images");
 const addFive = document.getElementById("add-5");
@@ -11,31 +12,31 @@ const startGame = document.getElementById("play");
 const Bank = document.querySelectorAll(".stored-wallet")[0];
 let playerWallet = 1000;
 let takehomeWallet = []
-let images = ['apple.jpg','banana.png', 'orange.png'];
+let images = ['banana.png','orange.png','cherries.jpg', 'apple.png', 'lemons.jpg','pear.jpg','watermelon.png'];
 
-
+// Slot machine play slot gets pressed and its a conditional statement
 function slotMachine(){
-  if(playerWallet <=5 ){ 
-    return wallet.innerHTML = `Please Deposit at least $5 to play`
-  } else {
+  if(playerWallet <=5 ){ // If the player wallet is less than 5 
+    return wallet.innerHTML = `Please Deposit at least $5 to play` // the function will stop and ask for $
+  } else { //otherwise 
 let randomPic1 = Math.floor(Math.random() * images.length);
 let randomPic2 = Math.floor(Math.random() * images.length);
 let randomPic3 = Math.floor(Math.random() * images.length);
-
+// from the array of images i made in the images vairable it will give me a random image and then
 slot1.src=`images/${images[randomPic1]}`
 slot2.src=`images/${images[randomPic2]}`
 slot3.src=`images/${images[randomPic3]}`
-
+// calling my constants by id of the img id's and concatenating the source img with the random picture
 if(randomPic1===randomPic2 && randomPic2===randomPic3){
   let msgWin = playerWallet +=50
-  return wallet.innerHTML = `Congratulations You Won, now your wallet is at $${msgWin}`
+  return wallet.innerHTML = `Congratulations You Won, now your wallet is at $${msgWin}`// if statement is winner
 }else{
   let msgLoss =  playerWallet -=5;
   return wallet.innerHTML = `Now your wallet is at $${msgLoss}`
   }
 }
 }
-
+// Functions to add to the player wallet by the amounts described 
 function add5() {
   let msg5 =  playerWallet +=5;
   return wallet.innerHTML = `Now your wallet is at $${msg5}`
@@ -50,15 +51,16 @@ function add100() {
   let msg100 = playerWallet +=100
   return wallet.innerHTML = `Now your wallet is at $${msg100}`
 }
+// Function to withdraw money and return a balance of the amount
 function withDraw() {
-  // withDrawWallet.innerHTML = `Your balance is ${playerWallet}`
-
+  takehomeWallet.push(playerWallet)
+  Bank.innerHTML = `Your Balance is $${takehomeWallet}`
   let msgTaken = playerWallet -= playerWallet
   return wallet.innerHTML = `Now your wallet is at $${msgTaken}, If you would like to play again please add to the wallet`
 }
 
 
-
+// Onclick functions to allows the game to be interactive
 startGame.onclick=()=>{
   slotMachine()
 }
